40adfe09d85595ea9dfda770d50cfa6bd929b0c3
Calum <6991398+0x63616c@users.noreply.github.com>
2026-06-12 06:32:03 -0700

    fix(captive-portal/CC-q002.26): add legacy bundle

--- apps/captive-portal/vite.config.ts
+++ apps/captive-portal/vite.config.ts
@@ -1,10 +1,17 @@
 import { resolve } from "node:path";
 import tailwindcss from "@tailwindcss/vite";
+import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    legacy({
+      targets: ["defaults", "not IE 11", "iOS >= 10"],
+    }),
+  ],
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
